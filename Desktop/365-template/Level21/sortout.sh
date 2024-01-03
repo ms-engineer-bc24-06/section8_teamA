b50a9de0359cf2294e3176084174434864432fb2
se1987 <s.tsukuda1987@gmail.com>
2024-01-03 09:52:23 +0900

    sortout.sh シェルスクリプトの修正

---
+++ Desktop/365-template/Level21/sortout.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# ダウンロードフォルダのパス
+DOWNLOADS_DIR="/Users/tsukudasayoko/downloads"
+
+# ダウンロードフォルダ内のファイルをループ処理
+for file in "$DOWNLOADS_DIR"/*; do
+    if [[ -f "$file" ]]; then
+        # ファイルの作成日を取得（%Y-%m-%d形式の日付）
+        file_date=$(date -r "$file" +%Y-%m-%d)
+
+        # ファイルの拡張子を取得
+        file_extension=${file##*.}
+
+        # 同じ名前のファイルの存在確認
+        if [ ! -e "$DOWNLOADS_DIR/$file_extension/$file_date" ]; then
+            # ファイルが存在する日付のフォルダを作成
+            mkdir -p "$DOWNLOADS_DIR/$file_extension/$file_date"
+        fi
+    
+        # ファイルを日付のフォルダに移動
+        mv -i "$file" "$DOWNLOADS_DIR/$file_extension/$file_date/"
+        echo "Moved $file to $DOWNLOADS_DIR/$file_extension/$file_date/"
+
+    else 
+        echo "移動が必要なファイルはありません。"
+    fi
+done
