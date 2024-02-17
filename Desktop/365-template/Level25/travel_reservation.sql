cbbcbb556d6263d48cc9faa4ac6101a66910b4d1
se1987 <s.tsukuda1987@gmail.com>
2024-02-17 19:02:02 +0900

    level25　課題１提出

---
+++ Desktop/365-template/Level25/travel_reservation.sql
@@ -0,0 +1,101 @@
+-- エンティティ作成
+/* 顧客情報 */
+-- 会員ランク、クーポン情報
+CREATE TABLE Memberships (
+    id INT PRIMARY KEY NOT NULL,
+    member_rank VARCHAR(20),
+    discount_rate DECIMAL NOT NULL
+);
+-- 名前、パスワード
+CREATE TABLE Users (
+    id INT PRIMARY KEY NOT NULL,
+    name VARCHAR(20) NOT NULL,
+    password VARCHAR(64) NOT NULL,
+    membership_id INT,
+    FOREIGN KEY (membership_id) REFERENCES Memberships(id)
+);
+
+/* 支払い */
+-- 支払い方法、支払い状況
+CREATE TABLE Payments (
+    id INT PRIMARY KEY NOT NULL,
+    payment_type VARCHAR(10) NOT NULL,
+    payment_status VARCHAR(5) NOT NULL
+);
+
+/* ホテル情報 */
+-- 部屋タイプ、空室情報
+CREATE TABLE RoomTypes (
+    id INT PRIMARY KEY NOT NULL,
+    room_type VARCHAR(20) NOT NULL,
+    available BOOLEAN
+);
+-- ホテル名
+CREATE TABLE Hotels (
+    id INT PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    room_type_id INT,
+    FOREIGN KEY (room_type_id) REFERENCES RoomTypes(id)
+);
+
+/* 予約状況 */
+-- キャンセル状況
+CREATE TABLE Cancellations (
+    id INT PRIMARY KEY NOT NULL,
+    cancellation BOOLEAN,
+    reason VARCHAR(100) 
+);
+-- 予約状況詳細
+CREATE TABLE Reservations (
+    id INT PRIMARY KEY NOT NULL,
+    user_id INT,
+    checkin DATE NOT NULL,
+    checkout DATE NOT NULL,
+    payment_id INT,
+    hotel_id INT,
+    cancellation_id INT,
+    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (payment_id) REFERENCES Payments(id),
+    FOREIGN KEY (hotel_id) REFERENCES Hotels(id),
+    FOREIGN KEY (cancellation_id) REFERENCES Cancellations(id)
+);
+
+-- テーブルの設定変更 => ALTER TABLE [テーブル名] ADD [追加カラム名] [型];
+    /* 予約テーブルにユーザーテーブルのidを外部キーとして追加する場合 */
+    ALTER TABLE Reservations
+    ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES Users(id);
+
+    /* クーポンデーブルの割引率のデータ型を固定小数点に変更する場合 */
+    ALTER TABLE Memberships
+    MODIFY COLUMN discount_rate DECIMAL;
+
+-- テーブルの削除 => DROP TABLE [テーブル名]
+    DROP TABLE Memberships;
+-- テーブルが存在する時のみテーブル削除 => DROP TABLE IF EXISTS [テーブル名]
+    DROP TABLE IF EXISTS Memberships;
+
+-- データの追加 => INSERT INTO [テーブル名] [フィールド名] VALUES [値]
+    /* 会員テーブルに３種類の会員情報を追加 */
+    -- 一行ずつ追加
+    INSERT INTO Memberships (id,member_rank,discount_rate) VALUES ('1','ゲスト','0.1');
+    INSERT INTO Memberships (id,member_rank,discount_rate) VALUES ('2','一般会員','0.3');
+    INSERT INTO Memberships (id,member_rank,discount_rate) VALUES ('3','プレミア会員','0.5');
+    -- まとめて追加
+    INSERT INTO Memberships VALUES ('1','ゲスト','0.1'),('2','一般会員','0.3'),('3','プレミア会員','0.5');
+
+-- データの削除 => TRUNCATE TABLE [テーブル名] ,DELETE FROM [テーブル名] WHERE [条件式]
+   /* 会員テーブル内の全データを削除 */
+   TRUNCATE TABLE Memberships;  --    ロールバック不可、処理速度が早い、条件指定不可、外部キー参照を解除しないと使えない
+   DELETE FROM Memberships;     --    ロールバック可、処理速度が遅い、条件指定可
+   /* 会員テーブル内の一般会員データを削除 */
+   DELETE FROM Memberships WHERE id = 2;
+
+-- データの更新 => UPDATE [テーブル名] SET [フィールド名]=[値] WHERE [条件式]
+   /* プレミア会員の割引率を更新 */
+   UPDATE Memberships SET discount_rate = 0.4 WHERE id = 3;
+
+
+
+
+
+
---
+++ Desktop/365-template/Level25/tweet.sql
@@ -0,0 +1,105 @@
+-- エンティティ作成
+/* 顧客情報 */
+-- 名前、メールアドレス、フォロー数、フォロワー数、パスワード
+CREATE TABLE Users (
+    id INT PRIMARY KEY NOT NULL,
+    name VARCHAR(20) NOT NULL,
+    email VARCHAR(40) NOT NULL,
+    password VARCHAR(64) NOT NULL
+);
+
+/* フォロー機能 */
+CREATE TABLE Relations (
+    id INT PRIMARY KEY NOT NULL,
+    followed_id INT NOT NULL,
+    followers_id INT NOT NULL,
+    FOREIGN KEY (followed_id) REFERENCES Users(id),
+    FOREIGN KEY (followers_id) REFERENCES Users(id)
+);
+
+/* つぶやき機能 */
+-- つぶやき投稿
+CREATE TABLE Tweets (
+    id INT PRIMARY KEY NOT NULL,
+    user_id INT NOT NULL,
+    tweet VARCHAR(100) NOT NULL,
+    date_time TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(id)
+);
+
+-- コメント機能
+CREATE TABLE Comments (
+    id INT PRIMARY KEY NOT NULL,
+    user_id INT NOT NULL,
+    comment VARCHAR(100) NOT NULL,
+    date_time TIMESTAMP NOT NULL,
+    tweet_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (tweet_id) REFERENCES Tweets(id)
+);
+
+-- いいね機能
+CREATE TABLE Favorites (
+    id INT PRIMARY KEY NOT NULL,
+    user_id INT NOT NULL,
+    tweet_id INT NOT NULL,
+    comment_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (tweet_id) REFERENCES Tweets(id),
+    FOREIGN KEY (comment_id) REFERENCES Comments(id)
+);
+
+/* DM機能 */
+-- ルームテーブル作成
+CREATE TABLE Rooms (
+    id INT PRIMARY KEY NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(id)
+);
+-- DMテーブル作成
+CREATE TABLE DMs (
+    id INT PRIMARY KEY NOT NULL,
+    user_id INT NOT NULL,
+    message VARCHAR(100) NOT NULL,
+    date_time TIMESTAMP NOT NULL,
+    room_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (room_id) REFERENCES Rooms(id)
+);
+
+-- 列定義の変更
+-- NULL値を許可する
+ALTER TABLE Favorites MODIFY COLUMN tweet_id INT;
+ALTER TABLE Favorites MODIFY COLUMN comment_id INT;
+
+-- データの追加
+    /* Usersテーブルに情報を追加 */
+    INSERT INTO Users VALUES (1,'Hitomi','Hitomi@mse.com','password1'),(2,'Sayoko','Sayoko@mse.com','password2'),(3,'Takumi','Takumi@mse.com','password3');
+    INSERT INTO Users VALUES (4,'Kaito','Kaito@mse.com','password4'),(5,'Souma','Souma@mse.com','password5'),(6,'Akari','Akari@mse.com','password6');
+    /* Tweetsテーブルに情報を追加 */
+    INSERT INTO Tweets VALUES (1,1,'やるか！超やるか！！',now()),(2,2,'モンブラン美味しい',now()),(3,3,'Zehi!',now()); 
+    /* Relationsテーブルに情報を追加 */
+    INSERT INTO Relations VALUES (1,1,2),(2,1,3),(3,1,4),(4,5,4),(5,3,2),(6,3,6),(7,2,1),(8,5,1),(9,5,2),(10,5,3); 
+    /* Commentsテーブルに情報を追加 */
+    INSERT INTO Comments VALUES (1,5,'お疲れ様です！',now(),1),(2,3,'美味しそう！',now(),2);
+    /* Favoritesテーブルに情報を追加 */ 
+    INSERT INTO Favorites VALUES (1,2,1,2),(2,2,1,NULL),(3,2,1,NULL),(4,3,2,NULL),(5,1,3,NULL),(6,1,3,NULL),(7,6,2,1),(8,5,3,1),(9,4,2,2),(10,4,2,NULL);
+
+-- ユーザーごとのフォロー数を集計する
+    -- LEFT JOIN を使用して Users テーブルと Relations テーブルを結合し、ユーザーごとのフォロー数を COUNT 関数を使って集計(以下同様にフォロワー数、いいね数を集計)
+SELECT u.id, u.name, COUNT(r.followed_id) AS follow_count
+FROM Users u
+LEFT JOIN Relations r ON u.id = r.followed_id
+GROUP BY u.id, u.name;
+
+-- ユーザーごとのフォロワー数を集計する
+SELECT u.id, u.name, COUNT(r.followers_id) AS followers_count
+FROM Users u
+LEFT JOIN Relations r ON u.id = r.followers_id
+GROUP BY u.id, u.name;
+
+-- ツイートごとのいいね数を集計する
+SELECT t.id, t.tweet, COUNT(f.id) AS favorite_count
+FROM Tweets t
+LEFT JOIN Favorites f ON t.id = f.tweet_id
+GROUP BY t.id, t.tweet;
